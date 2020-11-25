79e795920d8f953aed75a9644e0d244f78a7a07e
laura_at_wiris <laura@wiris.com>
2020-11-25 12:26:06 +0100

    lerna initial setup

---
+++ packages/library/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        node: true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    },
+    overrides: [
+        {
+            files: [
+                "**/__tests__/*.{j,t}s?(x)",
+                "**/tests/unit/**/*.spec.{j,t}s?(x)",
+                "./src/**/__mock__/*.{j,t}s",
+            ],
+        },
+    ],
+};
---
+++ packages/ui/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <div>
+      <label for="number1">First Number</label>
+      <input type="number" id="number1" v-model="number1" />
+    </div>
+    <div>
+      <label for="number2">Second Number</label>
+      <input type="number" id="number2" v-model="number2" />
+    </div>
+    <div>
+      <span>{{ add }}</span>
+    </div>
+    <div>
+      <span>{{ substract }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+import { sum, deduct } from "testlibrary";
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      number1: 0,
+      number2: 0,
+    };
+  },
+})
+export default class App extends Vue {
+  get add() {
+    console.log(typeof this.$data.number1);
+    return sum(+this.$data.number1, +this.$data.number2);
+  }
+  get substract(): number {
+    return deduct(this.$data.number1, this.$data.number2);
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
